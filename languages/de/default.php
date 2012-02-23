827adf993ce6777be1b650aaea94e8b3a9eb9fdb
Yanick Witschi <yanick.witschi@certo-net.ch>
2012-02-23 11:06:42 +0100

    german translation

---
+++ languages/de/default.php
@@ -0,0 +1,43 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Andreas Schempp 2011
+ * @author     Andreas Schempp <andreas@schempp.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC']['memberRegistrationDuplicate']	= 'Dieses Mitglied ist bereits angemeldet.';
+$GLOBALS['TL_LANG']['MSC']['eventRegister']					= 'Anmelden';
+$GLOBALS['TL_LANG']['MSC']['eventUnregister']				= 'Abmelden';
+$GLOBALS['TL_LANG']['MSC']['youAreRegistered']				= 'Sie sind für diesen Anlass angemeldet.';
+$GLOBALS['TL_LANG']['MSC']['youAreNotRegistered']			= 'Sie sind für diesen Anlass nicht angemeldet.';
+$GLOBALS['TL_LANG']['MSC']['registrationClosed']			= 'Die Anmeldung für diesen Anlass ist geschlossen.';
+$GLOBALS['TL_LANG']['MSC']['noMembersRegistered']			= 'Für diesen Anlass sind keine Mitglieder angemeldet.';
+$GLOBALS['TL_LANG']['MSC']['notSubscribed']					= 'Sie sind zurzeit für keinen Anlass angemeldet.';
+$GLOBALS['TL_LANG']['MSC']['unregisterSelected']			= 'Von den gewählten Anlässen abmelden';
+
---
+++ languages/de/tl_calendar_memberregistration.php
@@ -0,0 +1,52 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Andreas Schempp 2011
+ * @author     Andreas Schempp <andreas@schempp.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @version    $Id$
+ */
+ 
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['member']		= array('Mitglied', 'Bitte wählen Sie ein Mitglied für die Anmeldung zu diesem Anlass.');
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['registered']	= array('Angemeldet', 'Datum und Zeit für die Anmeldung dieses Mitglieds.');
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['disable']	= array('Abgemeldet', 'Dieses Mitglied hat seine Anmeldung zurückgezogen.');
+
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['csv']	= array('CSV Export', 'Anmeldungen als CSV Datei exportieren.');
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['new']	= array('Teilnehmer hinzufügen', 'Fügen Sie einen neuen Teilnehmer für diesen Anlass hinzu.');
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['delete']	= array('Teilnehmer entfernen', 'Entfernen Sie den Teilnehmer mit ID %s');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['member_legend']	= 'Mitglied';
+$GLOBALS['TL_LANG']['tl_calendar_memberregistration']['status_legend']	= 'Anmeldestatus';
+
